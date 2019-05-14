6be6fc308b2dbd94c3ab19bf2cff812d5cec7eaf
Morgan <morgan.gadiou@gmail.com>
2019-05-14 10:37:00 +0200

    Premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { PrimaryComponent } from './primary/primary.component';
+import { SecondaryComponent } from './secondary/secondary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    TopnavComponent,
+    PrimaryComponent,
+    SecondaryComponent
   ],
   imports: [
     BrowserModule,
