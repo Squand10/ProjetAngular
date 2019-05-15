921354576a184322540fd40ba081c3fe8c2ad505
Luc <luc.ameisen@yandex.com>
2019-05-15 15:07:06 +0200

    login and register in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import './polyfills';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -14,22 +17,44 @@ import { SecondaryComponent } from './secondary/secondary.component';
 import {DemoMaterialModule} from './material-module';
 import {SidenavService} from './sidenav/sidenav.service';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { routing } from './app.routing';
+
+import { JwtInterceptor } from './helpers/jwt.interceptor';
+import { ErrorInterceptor  } from './helpers/error.interceptor';
+import { fakeBackendProvider } from './helpers/fake-backend';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     TopnavComponent,
     PrimaryComponent,
-    SecondaryComponent
+    SecondaryComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-    DemoMaterialModule
+    DemoMaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [SidenavService],
+  providers: [
+    SidenavService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
